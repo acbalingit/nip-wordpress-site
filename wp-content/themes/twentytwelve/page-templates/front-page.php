1f0cd006f542cdfbaeff708feb1d0787053707e1
Chester Balingit <acbalingit@gmail.com>
2014-10-02 17:35:17 +0800

    change to template tloop

--- wp-content/themes/twentytwelve/page-templates/front-page.php
+++ wp-content/themes/twentytwelve/page-templates/front-page.php
@@ -31,14 +31,14 @@ get_header(); ?>
 			<div class='col-md-5 entry-content'>
 				<h1><a href='/category/announcement/'>News and Announcements</a></h1>
 				<?php query_posts('category_name=announcement&showposts=5'); ?>
-				<?php while ( have_posts() ) : the_excerpt(); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
             	<?php endwhile; // end of the loop. ?>
 			</div>
 		
             <div class='col-md-5 entry-content'>
 	            <h1><a href='/category/seminar/'>Seminars and Talks</a></h1>
 	            <?php query_posts('category_name=seminar&showposts=5'); ?>
-	            <?php while ( have_posts() ) : the_excerpt(); ?>
+	            <?php while ( have_posts() ) : the_post(); ?>
 	            <?php endwhile; // end of the loop. ?>
             </div>
 		</div>
