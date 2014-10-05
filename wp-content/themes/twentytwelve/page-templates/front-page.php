7de490518f6df8834d0fb4c902f124297cb5964a
Chester Balingit <acbalingit@gmail.com>
2014-10-05 12:52:35 +0800

    convert both to excerpt view

--- wp-content/themes/twentytwelve/page-templates/front-page.php
+++ wp-content/themes/twentytwelve/page-templates/front-page.php
@@ -14,53 +14,51 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
+    <div id="primary" class="site-content">
+        <div id="content" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php if ( has_post_thumbnail() ) : ?>
-					<div class="entry-page-image">
-						<?php the_post_thumbnail(); ?>
-					</div><!-- .entry-page-image -->
-				<?php endif; ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php if ( has_post_thumbnail() ) : ?>
+                    <div class="entry-page-image">
+                        <?php the_post_thumbnail(); ?>
+                    </div><!-- .entry-page-image -->
+                <?php endif; ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
-
-			<?php endwhile; // end of the loop. ?>
-		<div class='row'>
-			<div class='col-md-5 entry-content'>
-				<h1><a href='/category/announcement/'>News and Announcements</a></h1>
-				<?php query_posts('category_name=announcement&showposts=5'); ?>
-				<?php while ( have_posts() ) : the_post(); ?>
                 <?php get_template_part( 'content', 'page' ); ?>
-            	<?php endwhile; // end of the loop. ?>
-			</div>
-		
+
+            <?php endwhile; // end of the loop. ?>
+        <div class='row'>
             <div class='col-md-5 entry-content'>
-	            <h1><a href='/category/seminar/'>Seminars and Talks</a></h1>
-				<?php query_posts('category_name=seminar&showposts=5'); ?>
+                <h1><a href='/category/announcement/'>News and Announcements</a></h1>
+                <?php query_posts('category_name=announcement&showposts=5'); ?>
+                <?php if (have_posts()) : ?>
+                  <!-- <?php if (($wp_query->post_count) > 1) : ?> -->
+                    <?php while (have_posts()) : the_post(); ?>
+                    <!-- Do your post header stuff here for excerpts-->
+                    <?php the_excerpt() ?>
+                    <!-- Do your post footer stuff here for excerpts-->
+                    <?php endwhile; ?>
+                  <!-- <?php endif; ?> -->
+                <?php endif; ?>
+            </div>
+        
+            <div class='col-md-5 entry-content'>
+                <h1><a href='/category/seminar/'>Seminars and Talks</a></h1>
+                <?php query_posts('category_name=seminar&showposts=5'); ?>
+                <?php if (have_posts()) : ?>
+                  <!-- <?php if (($wp_query->post_count) > 1) : ?> -->
+                    <?php while (have_posts()) : the_post(); ?>
+                    <!-- Do your post header stuff here for excerpts-->
+                    <?php the_excerpt() ?>
+                    <!-- Do your post footer stuff here for excerpts-->
+                    <?php endwhile; ?>
+                  <!-- <?php endif; ?> -->
+                <?php endif; ?>
+            </div>
+        </div>
 
-				<?php
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
-
-				  /* Include the post format-specific template for the content. If you want to
-				   * this in a child theme then include a file called called content-___.php
-				   * (where ___ is the post format) and that will be used instead.
-				   */
-				  get_template_part( 'content', get_post_format() );
-
-				endwhile;
-
-				twentytwelve_content_nav( 'nav-below' );
-				?>
-
-
-	            </div>
-		</div>
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
+        </div><!-- #content -->
+    </div><!-- #primary -->
 
 <?php get_sidebar( 'front' ); ?>
 <?php get_footer(); ?>
